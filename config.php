2397ea393c715dfd564d8a31e408dfb8cfd2214d
Valerio2804 <valerio.mura.28@gmail.com>
2025-07-28 11:57:55 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// Configurazione utente
+$utente = 'admin';
+// La password "password123" è hashata con password_hash()
+$hash_password = '$2y$10$eB6Qxe8Y8Z6xHZz8CJLGBuyDzV99Sw6q305NlPN40jyX1YqJPRcuW';
+
+// Link da mostrare nell’area riservata
+$links = [
+    ['url' => 'https://example.com/link1', 'titolo' => 'Link 1'],
+    ['url' => 'https://example.com/link2', 'titolo' => 'Link 2'],
+];
+?>
